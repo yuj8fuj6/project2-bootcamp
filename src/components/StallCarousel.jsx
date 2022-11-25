2881681282a70b6f846473aca164209c7d06f107
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-11-25 11:21:52 +0800

    Added Partial Carousel

--- src/components/StallCarousel.jsx
+++ src/components/StallCarousel.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
 
 const StallCarousel = () => {
   return (
-    <div>StallCarousel</div>
-  )
-}
+    <div className="my-12 mx-auto">
+      <h2 className="text-4xl leading-8 font-semibold mb-12 text-slate-700">
+        Our epic carousel
+      </h2>
+      <div className="relative overflow-hidden">
+        <div className="flex justify-between absolute top left w-full h-full">
+          ...buttons
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default StallCarousel
+export default StallCarousel;
