4e0d5188f85ede3a47963391f27ec296dc24f7fe
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-11-29 02:27:26 +0800

    Added Order Form to Dish Page, added sample profile pics to public

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className="bg-purple text-white px-4 py-1 rounded-lg">{props.children}</button>
+    <button className="bg-purple text-white px-4 py-1 rounded-full" onClick={props.onClick}>{props.children}</button>
   )
 }
 
