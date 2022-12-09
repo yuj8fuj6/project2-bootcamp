37b4ffd7a902109ffcfeb85a96a386160d409a6b
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-12-10 01:08:23 +0800

    Completed Search Function for Landing Page

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export { default as Header } from "./Header";
 export { default as HeaderLogin } from "./HeaderLogin"
 export { default as NavBar } from "./NavBar";
-export { default as SearchFunction } from "./SearchFunction";
 export { default as StallCarousel } from "./StallCarousel";
 export { default as Button } from "./Button";
 export { default as DishCards } from "./DishCards";
