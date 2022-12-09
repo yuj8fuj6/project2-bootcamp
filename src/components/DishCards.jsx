b7277e291c4012d39ad2a72a2d194236f65250ba
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-12-09 19:44:31 +0800

    Completed dish cards for landing page - query orderByChild not working

--- src/components/DishCards.jsx
+++ src/components/DishCards.jsx
@@ -1,79 +1,75 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useCallback } from "react";
 import { BsHandThumbsUp, BsChatLeftText } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import {
+  ref as databaseRef,
+  getDatabase,
+  query,
+  orderByChild,
+  onValue,
+} from "firebase/database";
 
 const DishCards = () => {
-  const posts = [
-    {
-      title: "Prawn Noodles",
-      subtitle: "Prawn Village",
-      location: "Golden Mile Food Centre",
-      img: "/SampleDishPhotos/first-street-teochew-fish-soup-pomfret-fish-soup.jpg",
-      content: "Tantalizingly delicious! Would visit again, ...",
-    },
-    {
-      title: "Prawn Noodles",
-      subtitle: "Prawn Village",
-      location: "Golden Mile Food Centre",
-      img: "/SampleDishPhotos/first-street-teochew-fish-soup-pomfret-fish-soup.jpg",
-      content: "Tantalizingly delicious! Would visit again, ...",
-    },
-    {
-      title: "Prawn Noodles",
-      subtitle: "Prawn Village",
-      location: "Golden Mile Food Centre",
-      img: "/SampleDishPhotos/first-street-teochew-fish-soup-pomfret-fish-soup.jpg",
-      content: "Tantalizingly delicious! Would visit again, ...",
-    },
-    {
-      title: "Prawn Noodles",
-      subtitle: "Prawn Village",
-      location: "Golden Mile Food Centre",
-      img: "/SampleDishPhotos/first-street-teochew-fish-soup-pomfret-fish-soup.jpg",
-      content: "Tantalizingly delicious! Would visit again, ...",
-    },
-  ];
+  const [dishes, setDishes] = useState();
+  const [dishDetails, setDishDetails] = useState(null);
+
+  const fetchDishDetails = useCallback(() => {
+    const db = getDatabase();
+    const dishData = query(databaseRef(db, `dishes`), orderByChild(`dishName`));
+    onValue(dishData, (snapshot) => {
+      console.log(snapshot.val());
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        const [...dishes] = Object.values(snapshot.val());
+        setDishDetails(dishes);
+      }
+    });
+  }, [dishes]);
+
+  useEffect(() => {
+    fetchDishDetails();
+  }, [dishes]);
+
+  console.log(dishDetails);
+
   return (
     <>
       <div className="flex justify-evenly flex-wrap sm:flex-1 overflow-auto h-[32rem]">
-        {posts.map((items, key) => (
-          <Link to="dish">
-            <div
-              className="w-full rounded-lg shadow-md lg:max-w-sm hover:bg-orange/90 hover:opacity-75"
-              key={key}
-            >
-              <img
-                className="object-cover w-full h-72 p-2 rounded-2xl drop-shadow-xl"
-                src={items.img}
-                alt="dish-photo"
-              />
-              <div className="p-4 text-left">
-                <h4 className="text-lg font-extrabold text-purple">
-                  {items.title}
-                </h4>
-                <h5 className="text-sm font-extrabold text-purple">
-                  {items.subtitle}
-                </h5>
-                <h4 className="text-lg font-medium text-purple">
-                  {items.location}
-                </h4>
-                <p className="mb-2 leading-normal text-sm font-medium text-purple italic ">
-                  {items.content}
-                </p>
-              </div>
-              <div className="flex flex-wrap justify-start space-x-12 mx-5">
-                <div className="text-3xl font-semibold text-purple">
-                  <BsHandThumbsUp />
-                  <div className="text-xxs">Total Likes</div>
+        {dishDetails &&
+          dishDetails.map((item) => (
+            <Link to="dish">
+              <div className="w-full rounded-lg shadow-md lg:max-w-sm hover:bg-orange/90 hover:opacity-75">
+                <img
+                  className="object-cover w-full h-72 p-2 rounded-2xl drop-shadow-xl"
+                  src={item.photoURLs[0]}
+                  alt="dish-photo"
+                />
+                <div className="p-4 text-left">
+                  <h4 className="text-lg font-extrabold text-purple">
+                    {item.dishName}
+                  </h4>
+                  <h5 className="text-sm font-extrabold text-purple">
+                    {item.stallName}
+                  </h5>
+                  <p className="mb-2 leading-normal text-sm font-medium text-purple italic ">
+                    {item.story}
+                    {/* To place last review here */}
+                  </p>
                 </div>
-                <div className="text-3xl font-semibold text-purple">
-                  <BsChatLeftText />
-                  <div className="text-xxs">Total Reviews</div>
+                <div className="flex flex-wrap justify-start space-x-12 mx-5">
+                  <div className="text-3xl font-semibold text-purple">
+                    <BsHandThumbsUp />
+                    <div className="text-xxs">Total Likes</div>
+                  </div>
+                  <div className="text-3xl font-semibold text-purple">
+                    <BsChatLeftText />
+                    <div className="text-xxs">Total Reviews</div>
+                  </div>
                 </div>
-              </div>
-            </div>{" "}
-          </Link>
-        ))}
+              </div>{" "}
+            </Link>
+          ))}
       </div>
     </>
   );
