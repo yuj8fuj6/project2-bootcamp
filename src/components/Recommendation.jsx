4a3caa1640a6cb03d4623a2fcf57b45f4ae9da9f
LoyChaiEe <celoy001@gmail.com>
2022-12-13 16:22:05 +0800

    geohash is returning a promise, and cannot retrieve promiseresult

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -5,10 +5,21 @@ import Geocode from "react-geocode";
 import { useState, useEffect } from "react";
 var geohash = require("ngeohash");
 const dbRef = ref(getDatabase());
+
+// const neighboursOf = require('geohash-neighbours').neighboursOf
+// const northeastOf = require("geohash-neighbours").northeastOf;
+// const northOf = require("geohash-neighbours").northOf;
+// const northwestOf = require("geohash-neighbours").northwestOf;
+// const southwestOf = require("geohash-neighbours").southwestOf;
+// const southOf = require("geohash-neighbours").southOf;
+// const southeastOf = require("geohash-neighbours").southeastOf;
+
+
 Geocode.setApiKey(`${process.env.REACT_APP_GOOGLE_MAPS_API_KEYS}`);
 
 export default function Recommendation(props) {
   const [hawkerData, setHawkerData] = useState([]);
+  const [temp, setTemp] = useState({})
   //Get hawker's data from database
   useEffect(() => {
     async function getData() {
@@ -25,7 +36,7 @@ export default function Recommendation(props) {
           setHawkerData(
             data.map((hawker) => ({
               ...hawker,
-              geoHash: convertGeohash(hawker.stallAddress),
+              geoHash: getGeohash(hawker.stallAddress),
             }))
           );
         });
@@ -33,7 +44,23 @@ export default function Recommendation(props) {
     getData();
   }, []);
 
-  console.log(hawkerData);
+  console.log(hawkerData)
+
+  //Get Neightbours
+  // let neighbours = neighboursOf(geohash.encode(props.pos.lat, props.pos.lng, 6));
+  // neighbours.push(
+  //   northwestOf(neighbours[1]),
+  //   northOf(neighbours[1]),
+  //   northeastOf(neighbours[1]),
+  //   southwestOf(neighbours[6]),
+  //   southOf(neighbours[6]),
+  //   southeastOf(neighbours[6]),
+  // );
+
+  // let nearbyStalls = []
+  // for(let i = 0; i < hawkerData.length && ; i++)
+
+
 
   return (
     <div>
@@ -43,14 +70,23 @@ export default function Recommendation(props) {
   );
 }
 
-function convertGeohash(address) {
-  Geocode.fromAddress(address).then(
-    (response) => {
-      const { lat, lng } = response.results[0].geometry.location;
-      return geohash.encode(lat, lng, 6);
-    },
-    (error) => {
-      console.error(error);
-    }
-  );
+function getGeohash(address) {
+  let convertGeohash = async function (address) {
+    let response = await getLocation(address);
+    const { lat, lng } = response.results[0].geometry.location;
+    return geohash.encode(lat, lng, 6);
+  };
+
+  return convertGeohash(address)
 }
+
+// let convertGeohash = async function(address){
+//   let response = await getLocation(address)
+//   const { lat, lng } = response.results[0].geometry.location;
+//   return geohash.encode(lat, lng, 6)
+// }
+
+let getLocation = function(address){
+  return Geocode.fromAddress(address)
+}
+
