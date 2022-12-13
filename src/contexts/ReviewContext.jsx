c38fd24286744a347e70d77996824d7a25ed45b9
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-12-13 21:48:07 +0800

    Separated review cards, and added review context

--- src/contexts/ReviewContext.jsx
+++ src/contexts/ReviewContext.jsx
@@ -7,8 +7,29 @@ import {
   onChildAdded,
 } from "firebase/database";
 
-const ReviewContext = () => {
-  return <div>ReviewContext</div>;
-};
+export const ReviewContext = createContext();
 
-export default ReviewContext;
+export const ReviewContextProvider = (props) => {
+  const [reviewData, setReviewData] = useState([]);
+
+  useEffect(() => {
+    const db = getDatabase();
+    const reviewArr = [];
+    const reviewData = query(databaseRef(db, `reviews`), orderByChild(`user`));
+    onChildAdded(reviewData, (snapshot) => {
+      // console.log(snapshot.val())
+      const currentOrder = snapshot.val();
+      const currentOrderKey = snapshot.key;
+      reviewArr.push({ ...currentOrder, currentOrderKey });
+      setReviewData(reviewArr);
+    });
+  }, []);
+
+  console.log(reviewData);
+
+  return (
+    <ReviewContext.Provider value={reviewData}>
+      {props.children}
+    </ReviewContext.Provider>
+  );
+};
