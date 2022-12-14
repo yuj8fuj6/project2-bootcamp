b2b97064b97b9b2a36ce58a2f7bf7a53bfbbdf36
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2022-12-15 03:03:02 +0800

    Done like and review counter for user

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -2,11 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { ButtonDisabled } from "../components";
 import { BsHandThumbsUp, BsHandThumbsUpFill } from "react-icons/bs";
 import { database } from "../firebase";
-import {
-  ref as databaseRef,
-  update,
-  set,
-} from "firebase/database";
+import { ref as databaseRef, update, set } from "firebase/database";
 
 const FormReview = (props) => {
   const user = props.user;
@@ -17,11 +13,13 @@ const FormReview = (props) => {
   console.log(stall);
 
   const [review, setReview] = useState("");
-
+  const [wordCount, setWordCount] = useState(0);
   const [like, setLike] = useState(false);
 
   const handleReview = (e) => {
     setReview(e.target.value);
+    const charCount = e.target.value.length;
+    setWordCount(charCount);
   };
 
   const handleLike = () => {
@@ -33,28 +31,41 @@ const FormReview = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const reviewsListRef = databaseRef(
-      database,
-      `reviews/${dish.currentDishKey}/${user.uid}`,
-    );
-    const newReview = {
-      dishName: dish.dishName,
-      dishID: dish.currentDishKey,
-      stallName: stall.stallName,
-      hawkerID: stall.currentHawkerKey,
-      firstName: user.firstName,
-      contactEmail: user.contactEmail,
-      usertype: user.userType,
-      userID: user.uid,
-      date: Date(),
-      likes: 0,
-      content: review,
-    };
-    set(reviewsListRef, newReview).catch((error) => {
-      console.log(error);
-    });
-    
-    // update(databaseRef(database, ))
+    if (review.length !== 0) {
+      const reviewsListRef = databaseRef(
+        database,
+        `reviews/${dish.currentDishKey}/${user.uid}`,
+      );
+      const newReview = {
+        dishName: dish.dishName,
+        dishID: dish.currentDishKey,
+        stallName: stall.stallName,
+        hawkerID: stall.currentHawkerKey,
+        firstName: user.firstName,
+        contactEmail: user.contactEmail,
+        usertype: user.userType,
+        userID: user.uid,
+        date: Date(),
+        likes: 0,
+        content: review,
+      };
+      set(reviewsListRef, newReview).catch((error) => {
+        console.log(error);
+      });
+
+      update(databaseRef(database, `users/${user.uid}`), {
+        likesDone: user.likesDone + 1,
+        reviewsDone: user.reviewsDone + 1,
+      }).then(() => {
+        alert(
+          "Thank you for leaving a like and a review! It goes a long way towards helping the hawker!",
+        );
+      });
+    } else {
+      alert(
+        "Your like has not been submitted! Kindly leave a review to support the hawker.",
+      );
+    }
   };
 
   // console.log(review)
@@ -78,7 +89,7 @@ const FormReview = (props) => {
       <form className="grid grid-cols-1 justify-start mt-4">
         <label className="text-lg">Review: </label>
         <textarea
-          className="border-2 rounded-xl text-sm py-6 mt-2"
+          className="border-2 rounded-xl text-sm py-6 mt-2 indent-3"
           type="text"
           id="review"
           required
@@ -87,8 +98,9 @@ const FormReview = (props) => {
           onChange={handleReview}
           name="review"
           disabled={!like}
-          maxLength="200"
-        ></textarea>
+          maxLength={200}
+        />
+        <p className="text-xxs text-gray-700">Letter Count: {wordCount}/200</p>
       </form>
       <div className="flex justify-center m-3">
         <ButtonDisabled onClick={handleSubmit} user={user}>
