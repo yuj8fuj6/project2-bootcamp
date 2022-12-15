2c8e200217e724dbb6ee6cf83f302b599d0bb805
sharlearn <liewsharlen@gmail.com>
2022-12-15 18:49:57 +0800

    Added run transaction for handling review likes

--- src/components/ReviewCards.jsx
+++ src/components/ReviewCards.jsx
@@ -6,41 +6,58 @@ import {
 } from "react-icons/bs";
 import { UserContext } from "../App";
 import { ReviewContext } from "../contexts/ReviewContext";
-import { child, get, getDatabase, ref as databaseRef } from "firebase/database";
-import { database } from "../firebase";
-
-const getUserProfilePhoto = (userKey) => {
-  const url = "";
-  console.log("hello");
-  const dbRef = databaseRef(getDatabase());
-  get(child(dbRef, `users/${userKey}/profilePhoto`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        url = snapshot.val();
-      } else console.log("no photo");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  return url;
-};
+import {
+  child,
+  get,
+  getDatabase,
+  ref as databaseRef,
+  runTransaction,
+} from "firebase/database";
 
 const ReviewList = ({ review }) => {
-  console.log(review.userID);
-
   const [url, setURL] = useState();
+  const [like, setLike] = useState(false);
+  const user = useContext(UserContext);
+  const [currentReview, setCurrentReview] = useState(review);
 
   const dbRef = databaseRef(getDatabase());
   get(child(dbRef, `users/${review.userID}/profilePhoto`))
     .then((snapshot) => {
       if (snapshot.exists()) {
         setURL(snapshot.val());
-      } else console.log("no photo");
+      }
     })
     .catch((error) => {
       console.error(error);
     });
+
+  const handleLike = () => {
+    setLike(!like);
+    const reviewerKey = user.uid;
+
+    const reviewRef = databaseRef(
+      getDatabase(),
+      `/reviews/${review.dishID}/${review.userID}/`
+    );
+    console.log(reviewRef);
+
+    runTransaction(reviewRef, (post) => {
+      if (post) {
+        if (post.likers && post.likers[`${reviewerKey}`]) {
+          post.likes--;
+          post.likers[`${reviewerKey}`] = null;
+        } else {
+          post.likes++;
+          if (!post.likers) {
+            post.likers = {};
+          }
+          post.likers[`${reviewerKey}`] = true;
+        }
+        setCurrentReview(post);
+      }
+      return post;
+    });
+  };
 
   return (
     <div className="flex flex-wrap mb-3">
@@ -95,27 +112,35 @@ const ReviewList = ({ review }) => {
       <div className="grid grid-cols-1 w-4/6 pl-4 mt-4">
         <div className="font-extrabold">
           <p>
-            {review.firstName} -{" "}
-            <span className="italic">{review.usertype}</span>
+            {currentReview.firstName} -{" "}
+            <span className="italic">{currentReview.usertype}</span>
           </p>
-          <p className="font-normal">{review.date}</p>
+          <p className="font-normal">{currentReview.date}</p>
         </div>
         <div>
           <div className="flex flex-wrap justify-start space-x-2 mt-2 text-purple">
             <div className="text-3xl font-semibold">
-              <BsHandThumbsUp />
+              {like ? (
+                <button type="button" onClick={handleLike}>
+                  <BsHandThumbsUpFill />
+                </button>
+              ) : (
+                <button type="button" onClick={handleLike}>
+                  <BsHandThumbsUp />
+                </button>
+              )}
+              <div>{currentReview.likes}</div>
               <div className="text-xxs">Likes</div>
             </div>
-            <div>100</div>
-            <div className="text-3xl font-semibold">
+            {/* <div className="text-3xl font-semibold">
               <BsHandThumbsUp />
             </div>
             <div className="text-xxs w-1/3">
               Like this comment if you found it useful.
-            </div>
+            </div> */}
           </div>
         </div>
-        <div className="text-xs italic">"{review.content}"</div>
+        <div className="text-xs italic">"{currentReview.content}"</div>
       </div>
     </div>
   );
@@ -123,19 +148,13 @@ const ReviewList = ({ review }) => {
 
 const ReviewCards = ({ currentDish }) => {
   const { reviewObj } = useContext(ReviewContext);
-
-  console.log(currentDish);
-  console.log(reviewObj);
-
   const { currentDishKey } = currentDish;
 
   const dishReviewsList = Object.values(reviewObj[currentDishKey]);
-  console.log(dishReviewsList);
 
   return (
     <div className="border-t-1 w-11/12 border-purple text-purple text-left p-1 mt-2">
       <p className="text-xl font-semibold drop-shadow-lg mb-4">Reviews</p>
-      {/* <div className="overflow-scroll h-[32rem]">{reviewList}</div> */}
       <div className="overflow-scroll h-[32rem]">
         {dishReviewsList.map((review) => (
           <ReviewList review={review} />
